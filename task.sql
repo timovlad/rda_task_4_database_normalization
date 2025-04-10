f2fc5553b3d49fc1d80263dc3b6b5f9241d75a3d
timovlad <61474934+timovlad@users.noreply.github.com>
2025-04-10 20:45:49 +0200

    Update task.sql

--- task.sql
+++ task.sql
@@ -1,33 +1,40 @@
--- Create database and tables
-
 CREATE DATABASE ShopDB;
 USE ShopDB;
 
 CREATE TABLE Countries (
-    ID INT,
+    ID INT PRIMARY KEY,
+    Name VARCHAR(50)
+);
+
+CREATE TABLE Warehouses (
+    ID INT PRIMARY KEY,
     Name VARCHAR(50),
-    PRIMARY KEY (ID)
+    CountryID INT,
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION
+);
+
+CREATE TABLE WarehouseAddresses (
+    WarehouseID INT PRIMARY KEY,
+    Address VARCHAR(50),
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID)
 );
 
 CREATE TABLE ProductInventory (
-    ID INT,
+    ID INT PRIMARY KEY,
     ProductName VARCHAR(50),
     WarehouseAmount INT,
-    WarehouseName VARCHAR(50),
-    WarehouseAddress VARCHAR(50), 
-    CountryID INT,
-	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
-    PRIMARY KEY (ID)
+    WarehouseID INT,
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID)
 );
 
--- Populate test data
+INSERT INTO Countries (ID, Name) VALUES (1, 'Country1');
+INSERT INTO Countries (ID, Name) VALUES (2, 'Country2');
 
-INSERT INTO Countries (ID,Name)
-	VALUES (1, 'Country1');
-INSERT INTO Countries (ID,Name)
-	VALUES (2, 'Country2');
-    
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (1, 'AwersomeProduct', 2, 'Warehouse-1', 'City-1, Street-1',1);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (2, 'AwersomeProduct', 5, 'Warehouse-2', 'City-2, Street-2',2);
+INSERT INTO Warehouses (ID, Name, CountryID) VALUES (1, 'Warehouse-1', 1);
+INSERT INTO Warehouses (ID, Name, CountryID) VALUES (2, 'Warehouse-2', 2);
+
+INSERT INTO WarehouseAddresses (WarehouseID, Address) VALUES (1, 'City-1, Street-1');
+INSERT INTO WarehouseAddresses (WarehouseID, Address) VALUES (2, 'City-2, Street-2');
+
+INSERT INTO ProductInventory (ID, ProductName, WarehouseAmount, WarehouseID) VALUES (1, 'AwersomeProduct', 2, 1);
+INSERT INTO ProductInventory (ID, ProductName, WarehouseAmount, WarehouseID) VALUES (2, 'AwersomeProduct', 5, 2);
